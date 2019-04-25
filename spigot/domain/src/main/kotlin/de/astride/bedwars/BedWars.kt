c0b60354bfed542887760e483ae3ae7b1a72b817
LartyHD <lartyhd@gmail.com>
2019-04-25 09:34:50 +0200

    Added executeScripts support

--- spigot/domain/src/main/kotlin/de/astride/bedwars/BedWars.kt
+++ spigot/domain/src/main/kotlin/de/astride/bedwars/BedWars.kt
@@ -1,5 +1,6 @@
 package de.astride.bedwars
 
+import net.darkdevelopers.darkbedrock.darkness.general.functions.executeScripts
 import net.darkdevelopers.darkbedrock.darkness.spigot.plugin.DarkPlugin
 
 /**
@@ -9,5 +10,25 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.plugin.DarkPlugin
  */
 class BedWars : DarkPlugin() {
 
+    override fun onEnable(): Unit = onEnable {
+
+        var ns = System.nanoTime()
+
+        logger.info("Load scripts...")
+        executeScripts(
+            dataFolder.resolve("scripts"), mapOf(
+                "type" to "BedWars-Spigot",
+                "description" to description,
+                "javaplugin" to this
+            ),
+            before = {
+                ns = System.nanoTime()
+                logger.info("Load script ${it.name}...")
+            },
+            after = { logger.info("Loaded script ${it.name} (in ${ns}ns)") }
+        )
+        logger.info("Loaded scripts")
+
+    }
 
 }
