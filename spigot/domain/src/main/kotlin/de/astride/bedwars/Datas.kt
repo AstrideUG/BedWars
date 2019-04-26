440758e879071379ebd1f746ddf3687df19e4624
LartyHD <lartyhd@gmail.com>
2019-04-26 09:43:55 +0200

    Added teams to DatasKt

--- spigot/domain/src/main/kotlin/de/astride/bedwars/Datas.kt
+++ spigot/domain/src/main/kotlin/de/astride/bedwars/Datas.kt
@@ -4,6 +4,7 @@
 
 package de.astride.bedwars
 
+import net.darkdevelopers.darkbedrock.darkness.spigot.team.GameTeam
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
@@ -19,3 +20,17 @@ import org.bukkit.entity.Player
  * Current Version: 1.0 (26.04.2019 - 26.04.2019)
  */
 val players: Collection<Player> get() = Bukkit.getOnlinePlayers() //TODO: Bukkit.getOnlinePlayers().size - Saves.getTeamManager().getSpectators().size()
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 26.04.2019 03:11.
+ * Current Version: 1.0 (26.04.2019 - 26.04.2019)
+ */
+lateinit var teams: Set<GameTeam>
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 26.04.2019 05:51.
+ * Current Version: 1.0 (26.04.2019 - 26.04.2019)
+ */
+val Player.team: GameTeam? get() = teams.find { it.players.any { player -> player == this } }
