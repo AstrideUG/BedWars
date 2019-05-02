fdda05fd96400ce3cf6a186b09bd188742450994
LartyHD <lartyhd@gmail.com>
2019-05-02 06:18:57 +0200

    Updated Darkness and moved scripts to a function

--- spigot/domain/src/main/kotlin/de/astride/bedwars/BedWars.kt
+++ spigot/domain/src/main/kotlin/de/astride/bedwars/BedWars.kt
@@ -1,10 +1,12 @@
 package de.astride.bedwars
 
-import net.darkdevelopers.darkbedrock.darkness.general.functions.executeScripts
-import net.darkdevelopers.darkbedrock.darkness.general.functions.performCraftPluginUpdater
-import net.darkdevelopers.darkbedrock.darkness.general.functions.scriptEngineManager
+import com.google.gson.JsonObject
+import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
+import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonService.loadAs
+import net.darkdevelopers.darkbedrock.darkness.general.functions.*
 import net.darkdevelopers.darkbedrock.darkness.spigot.plugin.DarkPlugin
 import org.bukkit.Bukkit
+import java.io.File
 import javax.script.ScriptEngineManager
 
 /**
@@ -16,22 +18,39 @@ import javax.script.ScriptEngineManager
 class BedWars : DarkPlugin() {
 
     override fun onLoad(): Unit = onLoad {
-        performCraftPluginUpdater(
-            mapOf(
-                "type" to "DarkFrame-Spigot",
-                "description" to description,
-                "javaplugin" to this
-            )
+        val map = mapOf(
+            "type" to "DarkFrame-Spigot",
+            "description" to description,
+            "javaplugin" to this
         )
+        performCraftPluginUpdater(map)
     }
 
     override fun onEnable(): Unit = onEnable {
 
+        scripts()
+
+
+    }
+
+
+    private fun scripts() {
         var ns = System.nanoTime()
 
         scriptEngineManager = ScriptEngineManager(javaClass.classLoader)
 
         logger.info("Load scripts...")
+
+        val configData = ConfigData(dataFolder, "cashed-scripts.json")
+        val jsonObject = loadAs(configData) ?: JsonObject()
+        @Suppress("UNCHECKED_CAST")
+        val hashs = jsonObject.entrySet()
+            .map { it.key to it.value.asString() }
+            .mapNotNull { if (it.second == null) null else it }
+            .toMap() as Map<String, String>
+
+        val newHashs = mutableMapOf<String, String>()
+
         executeScripts(
             dataFolder.resolve("scripts"), mapOf(
                 "type" to "BedWars-Spigot",
@@ -39,14 +58,17 @@ class BedWars : DarkPlugin() {
                 "javaplugin" to this,
                 "Bukkit" to Bukkit::class.java
             ),
-            before = {
+            before = { file: File, _: String, _: String ->
                 ns = System.nanoTime()
-                logger.info("Load script ${it.name}...")
+                logger.info("Load script ${file.name}...")
             },
-            after = { logger.info("Loaded script ${it.name} (in ${ns}ns)") }
+            after = { file: File, input: String, hash: String ->
+                newHashs[file.name] = input.sha256()
+                logger.info("Loaded script ${file.name} (in ${ns}ns)")
+            },
+            hashs = hashs
         )
         logger.info("Loaded scripts")
-
     }
 
 }
