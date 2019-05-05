aa7bbb4872507e0cfdd4da74791294cdc86e4549
LartyHD <lartyhd@gmail.com>
2019-05-05 12:33:13 +0200

    Deleted teams

--- spigot/domain/src/main/kotlin/de/astride/bedwars/Datas.kt
+++ spigot/domain/src/main/kotlin/de/astride/bedwars/Datas.kt
@@ -4,7 +4,6 @@
 
 package de.astride.bedwars
 
-import net.darkdevelopers.darkbedrock.darkness.spigot.team.GameTeam
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
@@ -21,20 +20,6 @@ import org.bukkit.inventory.ItemStack
  * Current Version: 1.0 (26.04.2019 - 26.04.2019)
  */
 val players: Collection<Player> get() = Bukkit.getOnlinePlayers() //TODO: Bukkit.getOnlinePlayers().size - Saves.getTeamManager().getSpectators().size()
-
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 26.04.2019 03:11.
- * Current Version: 1.0 (26.04.2019 - 26.04.2019)
- */
-lateinit var teams: Set<GameTeam>
-
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 26.04.2019 05:51.
- * Current Version: 1.0 (26.04.2019 - 26.04.2019)
- */
-val Player.team: GameTeam? get() = teams.find { it.players.any { player -> player == this } }
 
 /**
  * @author Lars Artmann | LartyHD
